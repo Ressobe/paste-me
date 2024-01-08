9fa84e7e93cfacfdd69cb1ce545ad9dbbdfbb937
Bartosz Sobina <bartosz.sobina44@gmail.com>
2024-01-08 22:02:43 +0100

    add prisma

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,7 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+import type { PrismaClient } from "@prisma/client"
+
 declare global {
 	namespace App {
 		// interface Error {}
@@ -8,6 +10,7 @@ declare global {
 		// interface PageState {}
 		// interface Platform {}
 	}
+	var prisma: PrismaClient
 }
 
 export {};
